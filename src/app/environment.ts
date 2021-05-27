dfa779cedfa4c7b82c3a6fb4283cd179845863bd
AndresCM04 <carlosmosquera455@gmail.com>
2021-05-26 19:06:17 -0500

    1 file modified

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyD-3U8mxq8KE540120fE3Cz7WVcAJHGCgI",
-  authDomain: "fundamentos-web-2-39509.firebaseapp.com",
-  projectId: "fundamentos-web-2-39509",
-  storageBucket: "fundamentos-web-2-39509.appspot.com",
-  messagingSenderId: "232080548685",
-  appId: "1:232080548685:web:f4d59e8269b6446d764d04",
-  measurementId: "G-7WF7R85QF6"
+  apiKey: "AIzaSyCNAxuKzjbSjJTOFZc-A5J3x76N8VGPuT4",
+  authDomain: "fundamentos-web-2-e3e48.firebaseapp.com",
+  projectId: "fundamentos-web-2-e3e48",
+  storageBucket: "fundamentos-web-2-e3e48.appspot.com",
+  messagingSenderId: "371459112872",
+  appId: "1:371459112872:web:50f2086fef09e32ba0a79d",
+  measurementId: "G-Q4KDHPQP88"
 };
